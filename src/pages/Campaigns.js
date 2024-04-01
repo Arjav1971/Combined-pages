e3d87a85e6d8a5422e4a3a827f5ef7d30f5c1578
Arjav <1971arjavbarya@gmail.com>
2024-04-02 02:01:19 +0530

    correction in css of custom-dropdown

--- src/pages/Campaigns.js
+++ src/pages/Campaigns.js
@@ -8,9 +8,9 @@ import logodropdown from "../assets/Framedropdown.svg";
 import logoplus from "../assets/$dyte-iconplus.svg";
 import graph from "../assets/Graphgraph.svg";
 import logoup from "../assets/solar_arrow-up-boldup.svg";
-import logodropdownup from "../assets/Framelogoup.svg";
+import logodropdownup from  "../assets/Framelogoup.svg";
 
-import CustomDropdown from "../container/components/CustomDropdown";
+import CustomDropdown  from "../container/components/CustomDropdown";
 import ListBox from '../container/components/list-box';
 import { Layout, Menu, Button, theme, Dropdown } from 'antd';
 import campaignData from '../container/components/list-data/campaignData';
@@ -20,17 +20,17 @@ import { useNavigate } from 'react-router-dom';
 
 const submenu1 = (
   <Menu>
-    <Menu.Item className="custom-dropdown-key" key="1">Last Month</Menu.Item>
-    <Menu.Item className="custom-dropdown-key" key="2">Last 3 months</Menu.Item>
-    <Menu.Item className="custom-dropdown-key" key="3">Last 6 months</Menu.Item>
+    <Menu.Item  className="custom-dropdown-key" key="1">Last Month</Menu.Item>
+    <Menu.Item  className="custom-dropdown-key" key="2">Last 3 months</Menu.Item>
+    <Menu.Item  className="custom-dropdown-key" key="3">Last 6 months</Menu.Item>
   </Menu>
 );
 
 const submenu2 = (
   <Menu>
-    <Menu.Item className="custom-dropdown-key" key="1">Live Camapigns </Menu.Item>
-    <Menu.Item className="custom-dropdown-key" key="2">Ended Camapigns</Menu.Item>
-    <Menu.Item className="custom-dropdown-key" key="3">Upcoming Camapigns</Menu.Item>
+    <Menu.Item  className="custom-dropdown-key" key="1">Live Camapigns </Menu.Item>
+    <Menu.Item  className="custom-dropdown-key" key="2">Ended Camapigns</Menu.Item>
+    <Menu.Item  className="custom-dropdown-key" key="3">Upcoming Camapigns</Menu.Item>
   </Menu>
 );
 
@@ -39,6 +39,7 @@ const campaigns = campaignData;
 const Campaigns = () => {
     const [currentPage, setCurrentPage] = useState(1); // State for current page
     const [pageSize] = useState(5); // Number of items per page
+    const [searchTerm, setSearchTerm] = useState('');
   
     const handlePageChange = (page) => {
       setCurrentPage(page);
@@ -53,36 +54,45 @@ const Campaigns = () => {
       }
       console.log("Selected Box:", selectedBox); // Add this line to check selectedBox state
     };
-    // Calculate start and end index for current page
-    const startIndex = (currentPage - 1) * pageSize;
-    const endIndex = Math.min(startIndex + pageSize - 1, campaigns.length - 1);
-    // Get current page data
-    const currentPageData = campaigns.slice(startIndex, endIndex + 1);
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [isAddNewCampaignModalVisible, setIsAddNewCampaignModalVisible] = useState(false);
-
-  const handleEndCampaign = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalVisible(false);
-  };
-  const navigate = useNavigate();
-  const handleAddNewCampaign = () => {
-    navigate('/campaigns/new');
-  };
-
-    
 
 
+    const handleEndCampaign = () => {
+      setIsModalVisible(true);
+    };
+  
+    const handleCloseModal = () => {
+      setIsModalVisible(false);
+    };
+    const navigate = useNavigate();
+    const handleAddNewCampaign = () => {
+      navigate('/campaigns/new');
+    };
 
+    const handleSearchChange = (event) => {
+      setSearchTerm(event.target.value);
+    };
+   // Filter campaigns based on search term
+   const filteredCampaigns = campaignData.filter(campaign =>
+    campaign.heading.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  // Calculate start and end index for current page
+   const startIndex = (currentPage - 1) * pageSize;
+  // const endIndex = Math.min(startIndex + pageSize - 1, campaigns.length - 1);
+  const endIndex = Math.min(startIndex + pageSize - 1, filteredCampaigns.length - 1);
+  // Get current page data
+  // const currentPageData = campaigns.slice(startIndex, endIndex + 1);
+  const currentPageData = filteredCampaigns.slice(startIndex, endIndex + 1);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+
+  
+  
   return (
     <div className='campgn'>
             {/* <Content className="ant-layout"> */}
         <div className='submenu'>
           <div className="search-container">
-            <input type="text" className="form-control py-2" placeholder="Search by Campaign name" aria-label="Search by Campaign name" aria-describedby="basic-addon2"/>
+            <input type="text" className="form-control py-2" placeholder="Search by Campaign name" onChange={handleSearchChange} aria-label="Search by Campaign name" aria-describedby="basic-addon2"/>
             <img src={searchIcon} className="search-icon" alt="Search" />
           </div>
           <div className='right_options'>
@@ -95,15 +105,21 @@ const Campaigns = () => {
                 <p className='btn-text2'>New campaign</p>
                 
             </button>
+  
+
+
+            </div>
+
+
+
           </div>
         </div>
-      </div>
-      <div className='campaign-content'>
-        <div className='campaign-list'>
+        <div className='campaign-content'>
+          <div className='campaign-list'>
 
-          <div className='list-data'>
-            {currentPageData.map((campaign) => (
-              <ListBox
+            <div className='list-data'>
+                {currentPageData.map((campaign) => (
+                <ListBox
                 key={campaign.id}
                 id={campaign.id}
                 status={campaign.status}
@@ -113,143 +129,143 @@ const Campaigns = () => {
                 date={campaign.date}
                 isSelected={selectedBox === campaign.id}
                 handleBoxClick={handleBoxClick}
-              />
-            ))}
-          </div>
-          <div className='custom-pagination'>
-            <Pagination current={currentPage} pageSize={pageSize} total={campaigns.length} onChange={handlePageChange} />
-          </div>
+                />
+                ))}
+            </div>
+            <div className='custom-pagination'>
+                <Pagination current={currentPage} pageSize={pageSize} total={campaigns.length} onChange={handlePageChange}/>
+            </div>
 
-        </div>
-        <div className='campaign-details'>
+          </div>
+          <div className='campaign-details'>
           <div>
-            <button className='end-campaign' onClick={handleEndCampaign}>End Campaign</button>
+             <button className='end-campaign' onClick={handleEndCampaign}>End Campaign</button>
             <EndCampaignModal visible={isModalVisible} onClose={handleCloseModal} />
           </div>
+          
 
-
-          <div className='camapign-name'>
-            <div className='campaign-head'>
-              <div className='subheading'>
-                <div className='heading-grp'>
-                  <div className='camp-name'>Campaign Name 1</div>
-                  <div className='status' style={{ backgroundColor: '#27AE603D', color: '#219653' }}>
+            <div className='camapign-name'>
+              <div className='campaign-head'>
+                <div className='subheading'>
+                  <div className='heading-grp'>
+                    <div className='camp-name'>Campaign Name 1</div>
+                    <div className='status' style={{ backgroundColor: '#27AE603D', color: '#219653' }}>
                     Live
                   </div>
 
-                </div>
-
-
-
-              </div>
-              <div className='date-details'>
-                <div className='created-on'>Created on: 12th Feb, 2024</div>
-              </div>
-              <div className='date-details'>
-                <div className='created-on'>Ends on: 12th Mar, 2024</div>
-              </div>
-
-
-            </div>
-          </div>
-          <div className='campaign-data'>
-            <div className='container'>
-              <div className='box'>
-                <div className='box-head'>
-                  <div className='data'>
-                    <div className='data-head'>
-                      Appoinments Scheduled
-                    </div>
-                    <div className='count'>
-                      27
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-              <div className='box'>
-                <div className='box-head'>
-                  <div className='data'>
-                    <div className='data-head'>
-                      Appoinments Scheduled
-                    </div>
-                    <div className='count'>
-                      27
-                    </div>
                   </div>
 
 
 
                 </div>
-                <img className='box-img' src={graph} />
-
-
+                <div className='date-details'>
+                  <div className='created-on'>Created on: 12th Feb, 2024</div>
+                </div>
+                <div className='date-details'>
+                  <div className='created-on'>Ends on: 12th Mar, 2024</div>
+                </div>
+ 
+                
               </div>
-
-
             </div>
-            <div className='container'>
-              <div className='box'>
-                <div className='box-head'>
-                  <div className='data'>
-                    <div className='data-head'>
-                      Appoinments Scheduled
-                    </div>
-                    <div className='count'>
-                      27
+            <div className='campaign-data'>
+              <div className='container'>
+                <div className='box'>
+                  <div className='box-head'>
+                    <div className='data'>
+                      <div className='data-head'>
+                        Appoinments Scheduled
+                      </div>
+                      <div className='count'>
+                        27
+                      </div>
                     </div>
                   </div>
+
+                </div>
+                <div className='box'>
+                  <div className='box-head'>
+                    <div className='data'>
+                      <div className='data-head'>
+                        Appoinments Scheduled
+                      </div>
+                      <div className='count'>
+                        27
+                      </div>
+                    </div>
+                    
+                   
+                    
+                  </div>
+                  <img className='box-img' src={graph}/>
+
+
                 </div>
 
+
               </div>
-              <div className='box'>
-                <div className='box-head'>
-                  <div className='data'>
-                    <div className='data-head'>
-                      Appoinments Scheduled
-                    </div>
-                    <div className='count'>
-                      27
+              <div className='container'>
+                <div className='box'>
+                  <div className='box-head'>
+                    <div className='data'>
+                      <div className='data-head'>
+                        Appoinments Scheduled
+                      </div>
+                      <div className='count'>
+                        27
+                      </div>
                     </div>
                   </div>
+
                 </div>
-                <img className='box-img' src={graph} />
+                <div className='box'>
+                  <div className='box-head'>
+                    <div className='data'>
+                      <div className='data-head'>
+                        Appoinments Scheduled
+                      </div>
+                      <div className='count'>
+                        27
+                      </div>
+                    </div>
+                  </div>
+                <img className='box-img' src={graph}/>
+
+
+                </div>
 
 
               </div>
-
-
+              
             </div>
-
-          </div>
-          <data className='text-box'>
-            <div className='text-head' >
-              PURPOSE
-            </div>
-            <div className='text-content'>
-              Lorem ipsum dolor sit amet consectetur. Est magna condimentum maecenas varius est ullamcorper arcu. Ut semper tellus turpis in habitant fusce justo. Elit mattis lectus amet integer donec. Vitae id ultrices dictum quam nisi sit.
-            </div>
-          </data>
-          <data className='text-box'>
-            <div className='text-head' >
+            <data className='text-box'>
+              <div className='text-head' >
+                PURPOSE
+              </div>
+              <div className='text-content'>
+                Lorem ipsum dolor sit amet consectetur. Est magna condimentum maecenas varius est ullamcorper arcu. Ut semper tellus turpis in habitant fusce justo. Elit mattis lectus amet integer donec. Vitae id ultrices dictum quam nisi sit.
+              </div>
+            </data>
+            <data className='text-box'>
+              <div className='text-head' >
               GREETING MESSAGE
-            </div>
-            <div className='text-content'>
-              Lorem ipsum dolor sit amet consectetur. Est magna condimentum maecenas varius est ullamcorper arcu. Ut semper tellus turpis in habitant fusce justo. Elit mattis lectus amet integer donec. Vitae id ultrices dictum quam nisi sit.
-            </div>
-          </data>
-          <data className='text-box'>
-            <div className='text-head' >
+              </div>
+              <div className='text-content'>
+                Lorem ipsum dolor sit amet consectetur. Est magna condimentum maecenas varius est ullamcorper arcu. Ut semper tellus turpis in habitant fusce justo. Elit mattis lectus amet integer donec. Vitae id ultrices dictum quam nisi sit.
+              </div>
+            </data>
+            <data className='text-box'>
+              <div className='text-head' >
               TARGET USERS
-            </div>
-            <div className='text-content'>
-              Lorem ipsum dolor sit amet consectetur. Est magna condimentum maecenas varius est ullamcorper arcu. Ut semper tellus turpis in habitant fusce justo. Elit mattis lectus amet integer donec. Vitae id ultrices dictum quam nisi sit.
-            </div>
-          </data>
+              </div>
+              <div className='text-content'>
+                Lorem ipsum dolor sit amet consectetur. Est magna condimentum maecenas varius est ullamcorper arcu. Ut semper tellus turpis in habitant fusce justo. Elit mattis lectus amet integer donec. Vitae id ultrices dictum quam nisi sit.
+              </div>
+            </data>
+          </div>
         </div>
-      </div>
 
-
+       
       {/* </Content> */}
     </div>
   )
